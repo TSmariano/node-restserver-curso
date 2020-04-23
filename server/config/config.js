ba0b0fb0bc8be58f7814cd8d6c6b5005b9d48c39
TSmariano <47718531+TSmariano@users.noreply.github.com>
2020-04-23 14:51:55 -0600

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -4,5 +4,31 @@
 //      PUERTO
 //////////////////////////
 
+//  Constantes, variables de forma global
 
 process.env.PORT = process.env.PORT || 3000
+
+
+
+//////////////////////////
+//      Entorno
+//////////////////////////
+process.env.NODE_ENV =  process.env.NODE_ENV || 'dev';
+
+
+
+//////////////////////////
+//      Base de datos
+//////////////////////////
+let urlDB;
+
+if ( process.env.NODE_ENV === 'dev' ) {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else
+{
+    urlDB = 'mongodb+srv://tsmariano:WqhaqAGyNuvbef2f@cluster0-v025x.mongodb.net/cafe?retryWrites=true&w=majority';
+}
+
+process.env.URLDB = urlDB;
+
+
---
+++ server/models/usuario.js
@@ -0,0 +1,62 @@
+
+const mongoose = require('mongoose');
+
+const uniqueValidator = require('mongoose-unique-validator');
+
+// coleccion de roles validos y mensaje de error
+let rolesValidos = {
+    values: ['ADMIN_ROLE', 'USER_ROLE'],
+    message: '{VALUE} no es un rol valido'
+};
+
+
+let Schema = mongoose.Schema;
+
+//  Campos de la coleccion
+let usuarioSchema = new Schema({
+    nombre: {
+        type: String,
+        required: [true, 'El nombre es necesario']
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: [true, 'El correo es necesario']
+    },
+    password: {
+        type: String,
+        required: [true, 'La contraseña es obligatoria']
+    },
+    img: {
+        type: String,
+        required: false
+    },
+    role: {
+        type: String,
+        default: 'USER_ROLE',
+        enum: rolesValidos
+    },
+    estado: {
+        type: Boolean,
+        default: true
+    },
+    google: {
+        type: Boolean,
+        default: false
+    }
+});
+
+usuarioSchema.methods.toJSON = function() {
+    let user = this;
+    let userObject = user.toObject();
+    delete userObject.password;
+
+
+    return userObject;
+}
+
+// valida correo unico y mueestra mensaje de error
+usuarioSchema.plugin(uniqueValidator, { message: '{PATH} debe ser unico' });
+
+module.exports = mongoose.model('Usuario', usuarioSchema);
+
